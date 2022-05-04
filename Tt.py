9aabf43aa1f492b63c72d039cf4bafd73931a3ee
Charan C <72061061+ccharan@users.noreply.github.com>
2022-05-04 10:39:20 +0530

    Update Tt.py

--- Tt.py
+++ Tt.py
@@ -21,3 +21,66 @@ for i in range(1,testModules.Count+1):#read all test modules
         #print(testCase.Name)#printing testcase
      
     test_Module.Start()#starting testcase execution
+
+    
+    
+    
+    import win32com.client as win32
+
+if __name__ == '__main__':
+    
+    '''Launch'''
+    CANoe = win32.DispatchEx("CANoe.Application")
+    '''Load Configuration'''
+    CANoe.Open("C:\\Users\\Public\\Documents\\Vector\\CANoe\\10.0\\CANoe Sample Configurations\\CAN\\TestFeatureSet\\CentralLockingSystem\\CentralLockingSystem.cfg")
+    '''Start Measurement'''    
+    CANoe.Measurement.Start()
+    
+    systemCAN = CANoe.System.Namespaces
+    sys_namespace = systemCAN("SystemUnderTest")
+    sysVariables = sys_namespace.Variables
+    print(type(sysVariables))
+    
+    for i in range(1, sysVariables.Count+1):
+        sysVar = win32.CastTo(sysVariables.Item(i), "IVariable")
+        print(sysVar.Name)
+   
+    sys_value = sysVariables("ErrorInCrashSensorUsage")
+    sys_value.Value = 1
+    
+    sys_value2 = sys_namespace.Variables("ErrorCrashSensorOnVelocity")
+    sys_value2.Value = 1
+    
+    signalValue = CANoe.GetBus("CAN").GetSignal(1, "VehicleMotion", "EngineRunning")
+    engineRunning = win32.CastTo(signalValue, "ISignal4")
+    print(engineRunning.Value)
+    
+#     print(type(CANoe.Environment.getVariable("").Value)
+    
+    while engineRunning.Value == 0:
+        print(CANoe.GetBus("CAN").GetSignal(1,"VehicleMotion","EngineRunning"))
+
+    systemCAN = CANoe.System.Namespaces
+    sys_namespace = systemCAN("SystemUnderTest")
+    sys_value = sysVariables("ErrorInCrashSensorUsage")
+    print(sys_value.Value)
+    
+    testSetup = CANoe.Configuration.TestSetup
+    test_env = testSetup.TestEnvironments.Item(1)
+    test_env = win32.CastTo(test_env, "ITestEnvironment2")
+    print(test_env.Name)
+    
+    testModules = test_env.TestModules
+    result = 0
+    for i in range(1, testModules.Count+1):
+        test_module = test_env.TestModules.Item(i)
+        print(test_module.Name)
+        
+        seq = test_module.Sequence
+        for i in range(1, seq.Count + 1):
+            tc = win32.CastTo(seq.Item(i), "ITestCase")
+            print(tc.Name)
+            
+        test_module.Start()
+    
+    
